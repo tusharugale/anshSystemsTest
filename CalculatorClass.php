75527510675da8795fbadce74a8862696a232051
Tushar Ugale <tusharugale.work@gmail.com>
2018-07-20 12:20:33 +0530

    Task 3 Done

--- CalculatorClass.php
+++ CalculatorClass.php
@@ -18,13 +18,37 @@ class CalculatorClass{
 			$params = explode(',', $parameters[0]);
 			if(!empty($params)){
 				foreach ($params as $param) {
-					if($param != ''){
-						$sum += $param;					
-					}
+					$special_case = $this->checkForSpecialChars($param);
+					if($special_case){
+						if($special_case == 'SLASHN'){
+							$sum += $this->getSlashNSum($param);
+						}
+					}else{
+						if($param != ''){
+							$sum += $param;					
+						}
+					}				
+					
 				}
 			}	
 		}			
 		echo $sum;
+	}
+
+	public function checkForSpecialChars($string){
+		if (strpos($string, '\n') !== false) {
+			return 'SLASHN';
+		}
+		return false;
+	}
+
+	public function getSlashNSum($string){
+		$sum = 0;
+		$parts = explode('\n', $string);
+		foreach ($parts as $part) {
+			$sum += $part;
+		}
+		return $sum;
 	}
 	
 }
